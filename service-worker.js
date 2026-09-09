dbc35bc2a636afdc5b8ea8e873f5056406daa788
Neighbourly-spec <fortuin.courtney@gmail.com>
2026-09-09 09:39:39 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='neighbourly-v0-4-2';
+const CACHE='neighbourly-v0-4-3';
 const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','neighbourly-quick-start-guide.pdf','sisters-help-desk.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
